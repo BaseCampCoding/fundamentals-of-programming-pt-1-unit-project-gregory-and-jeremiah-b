8d1ed7b98e8fc4d91bb43ae340a0ab32e34177a5
Jeremiah <72397368+BlueBass1@users.noreply.github.com>
2020-10-06 15:31:03 -0500

    Update Poké_Database_B.py

--- Poké_Database_B.py
+++ Poké_Database_B.py
@@ -1,13 +1,6 @@
 #Bulbasaur, Charmander, Squirtle, Pikachu, Chikorita
 #Cyndaquil, Totodile, Treecko, Torchic, Mudkip
 print("Here are the available starters\n(articuno)")
-Articuno = [
-    144,
-    ("Ice", "flying"),
-    "Doesn't evolve",
-    "Articuno is a legendary bird Pokémon that can control ice."
-    "The flapping of its wings chills the air. As a result, it is said that when this Pokémon flies, snow will fall."
-]
 while True:
     response = input("What starter would you like to view? (gens 1-3) (q to quit) : ").lower()
     if response == 'articuno':
