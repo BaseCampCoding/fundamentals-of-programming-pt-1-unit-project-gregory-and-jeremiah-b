dc07dfe47674fdff6c6d6751aa3267f5a8090b77
Jeremiah <72397368+BlueBass1@users.noreply.github.com>
2020-10-06 16:11:58 -0500

    Update Poké_Database_B.py

--- Poké_Database_B.py
+++ Poké_Database_B.py
@@ -1,17 +1,51 @@
-#Bulbasaur, Charmander, Squirtle, Pikachu, Chikorita
-#Cyndaquil, Totodile, Treecko, Torchic, Mudkip
+#bulbasaur, charmander, squirtle, pikachu, chikorita
+#cyndaquil, totodile, treecko, torchic, mudkip
 print("Here are the available starters\n(articuno)")
 while True:
     response = input("What starter would you like to view? (gens 1-3) (q to quit) : ").lower()
     if response == 'articuno':
-        print(Articuno[:5])
+        print(cyndaquil[:5])
         break
     elif response == 'q':
         break
-Cyndaquil=[
+cyndaquil=[
     155,
     ("Fire"),
     "evolves into Quilava",
     "It is timid, and always curls itself up in a ball."
     "If attacked, it flares up its back for protection"
     ]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+bulbasaur =
