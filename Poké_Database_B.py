b98d9e3d2713906e69134fb8ad02e4a75ae243d3
Gregory Bowles <Sbowles21@users.noreply.github.com>
2020-10-07 15:46:32 -0500

    moved a print into a while loop

--- Poké_Database_B.py
+++ Poké_Database_B.py
@@ -12,8 +12,9 @@ while True:
     break
     print("Not a Starter from available starters.")
 
-print(f"Here are the types{Data.types[0]}")
+
 while True:
+   print(f"Here are the types{Data.types[0]}")
    Type_categories = input("Pick a type for a list of Pokemon in that type. (gens 1-3) (q to quit) :")
    for i in Data.starters[int(0)]:
       if Type_categories in Data.types:
