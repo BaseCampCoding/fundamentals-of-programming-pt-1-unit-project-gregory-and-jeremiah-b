ded0686535cb304e20e5d255f96c4a9f69a99089
Gregory Bowles <Sbowles21@users.noreply.github.com>
2020-10-07 15:59:03 -0500

    Got it to print the list

--- Poké_Database_B.py
+++ Poké_Database_B.py
@@ -6,9 +6,8 @@ print(f"Here are the available starters{Data.Starters_string[0]}")
 while True:
     Is_Starter = input("What starter would you like to view? (gens 1-3): ")
     for i in Data.starters:
-     if Is_Starter in i:
-        Starter_index = i.index(Is_Starter)
-        print()
+     if i[0]== Is_Starter:
+        print(i)
     break
     print("Not a Starter from available starters.")
 
