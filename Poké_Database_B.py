4a029cb8c78cd54b6cbd27298d502cd51e609601
Gregory Bowles <Sbowles21@users.noreply.github.com>
2020-10-07 15:13:09 -0500

    I made the list more searchable

--- Poké_Database_B.py
+++ Poké_Database_B.py
@@ -5,10 +5,10 @@ print(f"Here are the available starters{Data.Starters[0]}")
 
 while True:
     Is_Starter = input("What starter would you like to view? (gens 1-3) (q to quit) : ")
-    for 
-    if Is_Starter in Data.starters:
+    for i in Data.starters[int(0)]:
+     if Is_Starter in Data.starters:
         Starter_index = Data.starters.index(Is_Starter)
         break
-    else:
+     else:
         print("Not a Starter from available starters.")
 
