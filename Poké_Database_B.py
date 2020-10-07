da37ebfa9c121f7734c4f6a9ac3bce7f2e9c5406
Jeremiah <72397368+BlueBass1@users.noreply.github.com>
2020-10-07 16:31:26 -0500

    Added types

--- Poké_Database_B.py
+++ Poké_Database_B.py
@@ -1,7 +1,7 @@
 import Data
 
 
-print(f"Here are the available starters{Data.Starters_string[0]}")
+print(f"Here are the available starters{Data.Starter[0]}")
 
 while True:
     Is_Starter = input("What starter would you like to view? (gens 1-3): ")
@@ -13,11 +13,9 @@ while True:
 
 
 while True:
-   print(f"Here are the types{Data.types[0]}")
-   Type_categories = input("Pick a type for a list of Pokemon in that type. (gens 1-3) (q to quit) :")
-   for i in Data.starters[int(0)]:
-      if Type_categories in Data.types:
-         Type_index = Data.types.index(Type_categories)
-         break
-      else:
-         print("Not a valid type. Please choose valid types")
+    Type_categories = input("Use a valid type to get a list of pokemon in requested type. (gens 1-3): ")
+    for i in Data.starters:
+     if i[2]== Is_Starter:
+        print(i)
+    break
+    print("Not a valid type. Please choose valid types")
