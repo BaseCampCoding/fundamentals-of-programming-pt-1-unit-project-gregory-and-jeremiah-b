07b146e0ec2478472582ac74b10963bb78880a4d
Jeremiah <72397368+BlueBass1@users.noreply.github.com>
2020-10-07 15:26:34 -0500

    I added my loop w.i.p

--- Poké_Database_B.py
+++ Poké_Database_B.py
@@ -1,7 +1,7 @@
 import Data
 
 
-print(f"Here are the available starters{Data.Starters[0]}")
+print(f"Here are the available starters{Data.Starters_string[0]}")
 
 while True:
     Is_Starter = input("What starter would you like to view? (gens 1-3) (q to quit) : ")
@@ -12,3 +12,12 @@ while True:
      else:
         print("Not a Starter from available starters.")
 
+print(f"Here are the types{Data.types[0]}")
+while True:
+   Type_categories = input("Pick a type for a list of Pokemon in that type. (gens 1-3) (q to quit) :")
+   for i in Data.starters[int(0)]:
+      if Type_categories in Data.types:
+         Type_index = Data.types.index(Type_categories)
+         break
+      else:
+         print("Not a valid type. Please choose valid types")
