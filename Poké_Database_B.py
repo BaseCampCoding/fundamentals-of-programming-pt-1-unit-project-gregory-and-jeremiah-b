ca7b223cf0b2f511dba9833f811f9b30be9ddbc3
Jeremiah <72397368+BlueBass1@users.noreply.github.com>
2020-10-07 16:05:33 -0500

    mistake in code

--- Poké_Database_B.py
+++ Poké_Database_B.py
@@ -4,18 +4,16 @@ import Data
 print(f"Here are the available starters{Data.Starters_string[0]}")
 
 while True:
-   Is_Starter = input("What starter would you like to view? (gens 1-3): ")
-   for i in Data.starters:
-      if Is_Starter in i:
-         Starter_index = i.index(Is_Starter)
-         print()
-         break
-      else:
-         print("Not a Starter from available starters.")
+    Is_Starter = input("What starter would you like to view? (gens 1-3): ")
+    for i in Data.starters:
+     if Is_Starter in i:
+        Starter_index = i.index(Is_Starter)
+        print()
+    break
+print("Not a Starter from available starters.")
 
-
+print(f"Here are the types{Data.types[0]}")
 while True:
-   print(f"Here are the types{Data.types[0]}")
    Type_categories = input("Pick a type for a list of Pokemon in that type. (gens 1-3) (q to quit) :")
    for i in Data.starters[int(0)]:
       if Type_categories in Data.types:
