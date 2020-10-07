6c828340a84c26d208cee42d9d0cb53848d7da52
Jeremiah <72397368+BlueBass1@users.noreply.github.com>
2020-10-07 15:39:50 -0500

    Fixed Indentation Error

--- Poké_Database_B.py
+++ Poké_Database_B.py
@@ -4,13 +4,14 @@ import Data
 print(f"Here are the available starters{Data.Starters_string[0]}")
 
 while True:
-    Is_Starter = input("What starter would you like to view? (gens 1-3): ")
-    for i in Data.starters:
-     if Is_Starter in i:
-        Starter_index = i.index(Is_Starter)
-        print()
-    break
-    print("Not a Starter from available starters.")
+   Is_Starter = input("What starter would you like to view? (gens 1-3): ")
+   for i in Data.starters:
+      if Is_Starter in i:
+         Starter_index = i.index(Is_Starter)
+         print()
+         break
+      else:
+         print("Not a Starter from available starters.")
 
 print(f"Here are the types{Data.types[0]}")
 while True:
