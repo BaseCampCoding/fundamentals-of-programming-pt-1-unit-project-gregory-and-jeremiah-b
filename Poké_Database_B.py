2ad728d6cbae77150790a50b0b77cac8976211bb
Gregory Bowles <Sbowles21@users.noreply.github.com>
2020-10-08 11:22:04 -0500

    Update Poké_Database_B.py

--- Poké_Database_B.py
+++ Poké_Database_B.py
@@ -4,14 +4,14 @@ import Data
 print(f"Here are the available starters{Data.Starters_string[0]}")
 
 while True:
-    Is_Starter = input("What starter would you like to view? (gens 1-3): ")
+    Is_Starter = input("What starter would you like to view? (gens 1-3): ").lower()
     for i in Data.Starters:
-     if i[0]== Is_Starter:
+     if i[0] == Is_Starter:
          print(i)
          break
      elif Is_Starter == "q":
          quit()
-     elif i[0]!= Is_Starter:
+     elif i[0] != Is_Starter and i == Data.Starters[-1]:
          print("Not a Starter from available starters! Please choose a valid starter.")
 
 
