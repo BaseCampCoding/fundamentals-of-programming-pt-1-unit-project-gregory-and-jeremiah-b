a29fd04bfac05361d6da600a01b9ad820b0a6d04
Gregory Bowles <Sbowles21@users.noreply.github.com>
2020-10-08 15:54:48 -0500

    Update Poké_Database_B.py

--- Poké_Database_B.py
+++ Poké_Database_B.py
@@ -1,17 +1,18 @@
 import Data
-
-while True:
+Awnser=input("What do you want to search by? starters,types,weaknesses,generations")
+if Awnser == "starters":
+    while True:
     Is_Starter = input("What starter would you like to view? (gens 1-3) (q to quit):").lower()
     for i in Data.Starters:
      if i[0] == Is_Starter:
         print(i)
         break
      elif Is_Starter == "q":
-         quit()
+        quit()
      elif i[0] != Is_Starter and i == Data.Starters[-1]:
          print("Not a Starter from available starters! Please choose a valid starter.")
-
-while True:
+elif Awnser == "types":
+    while True:
     check_if_found_valid = False
     Type_categories = input("Use a valid type to get a list of pokemon in requested type. (gens 1-3) (press 'q' when done searching): ")
     for i in Data.Starters:
@@ -22,3 +23,11 @@ while True:
          quit() 
      elif i[2] != Type_categories and i == Data.Starters[-1] and check_if_found_valid == False:
         print("Not a valid type. Please choose valid types")
+elif Awnser == "weaknesses":
+    Weaknesses_types=['weak to water, weak to grass, weak to fire']
+while True:
+    Weaknesses = input(f"What type do you want to see the advantage of? {Weaknesses_types} (q to quit): \n").lower()
+    for i in Data.Starters:
+     if i[3] == Weaknesses:
+        print(i[0] + " is " + Weaknesses)
+elif Awnser == "generations":
