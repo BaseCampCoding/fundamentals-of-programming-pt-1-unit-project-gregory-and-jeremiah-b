a3232389d3ecfd8029ee653436a540fad33dc955
Jeremiah <72397368+BlueBass1@users.noreply.github.com>
2020-10-08 15:51:05 -0500

    Added types categories and gens

--- Poké_Database_B.py
+++ Poké_Database_B.py
@@ -1,8 +1,5 @@
 import Data
 
-
-print(f"Here are the available starters{Data.Starters_string[0]}")
-
 while True:
     Is_Starter = input("What starter would you like to view? (gens 1-3) (q to quit):").lower()
     for i in Data.Starters:
@@ -13,16 +10,15 @@ while True:
          quit()
      elif i[0] != Is_Starter and i == Data.Starters[-1]:
          print("Not a Starter from available starters! Please choose a valid starter.")
-         
-
 
 while True:
-    Type_categories = input("Use a valid type to get a list of pokemon in requested type. (gens 1-3): ")
+    check_if_found_valid = False
+    Type_categories = input("Use a valid type to get a list of pokemon in requested type. (gens 1-3) (press 'q' when done searching): ")
     for i in Data.Starters:
-     if i[2]== Type_categories:
-        print(i)
-        break
+     if i[2] == Type_categories:
+         print(i[0])
+         check_if_found_valid = True
      elif Type_categories == "q":
          quit() 
-     elif i[2]== Type_categories:
+     elif i[2] != Type_categories and i == Data.Starters[-1] and check_if_found_valid == False:
         print("Not a valid type. Please choose valid types")
