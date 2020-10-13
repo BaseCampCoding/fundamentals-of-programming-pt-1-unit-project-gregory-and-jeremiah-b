0537f2ba41680434f866f274e2f54d487a559851
Gregory Bowles <Sbowles21@users.noreply.github.com>
2020-10-13 15:24:45 -0500

    Update Poké_Database_B.py

--- Poké_Database_B.py
+++ Poké_Database_B.py
@@ -1,49 +1,51 @@
 import Data
-Awnser=input("What do you want to search by? names,types,weaknesses,generations\n")
-if Awnser == "names":
-    while True:
-        print(f"Here are the available starters{Data.Starters_string[0]}")
-        Is_Starter = input("What starter would you like to view? (gens 1-3) (q to quit): ").lower()
-        for i in Data.Starters:
-            if i[0] == Is_Starter:
-                print(i)
-                break
-            elif Is_Starter == "q":
-                quit()
-            elif i[0] != Is_Starter and i == Data.Starters[-1]:
+while True:
+    Awnser=input("What do you want to search by? names,types,weaknesses,generations\n")
+    if Awnser == "names":
+        while True:
+            print(f"Here are the available starters{Data.Starters_string[0]}")
+            Is_Starter = input("What starter would you like to view? (gens 1-3) (q to quit): ").lower()
+            if Is_Starter == "q":
+                    break
+            for i in Data.Starters:
+                if i[0] == Is_Starter:
+                    print(i)
+                elif i[0] != Is_Starter and i == Data.Starters[-1]:
                     print("Not a Starter from available starters! Please choose a valid starter.")
-elif Awnser == "types":
-    while True:
-        check_if_found_valid = False
-        Type_categories = input("Use a valid type to get a list of pokemon in requested type. (gens 1-3) (press 'q' to quit): ").lower()
-        for i in Data.Starters:
-            if i[2] == Type_categories:
-                print(i[0])
-                check_if_found_valid = True
-            elif Type_categories == "q":
-                quit() 
-            elif i[2] != Type_categories and i == Data.Starters[-1] and check_if_found_valid == False:
-                print("Not a valid type. Please choose valid types")
-elif Awnser == "weaknesses":
-    Weaknesses_types=['weak to water, weak to grass, weak to fire']
-    while True:
-        Weaknesses = input(f"What type do you want to see the advantage of? {Weaknesses_types} (q to quit): \n").lower()
-        for i in Data.Starters:
-            if i[4] == Weaknesses:
-                print(i[0] + " is " + Weaknesses)
-            elif Weaknesses == "q":
-                quit()
-            elif i[4] != Weaknesses and i == Data.Starters[-1]:
-                print ("Not a valid weakness. Please choose a valid weakness")
-elif Awnser == "generations":
-    while True:
-        check_if_found_valid = False
-        generations = input("Pick a generation for a list of pokemon. (gens 1-3) (press 'q' to quit): ").lower()
-        for i in Data.Starters:
-            if i[3] == generations:
-                print(i[0])
-                check_if_found_valid = True
-            elif generations == "q":
-                quit() 
-            elif i[2] != generations and i == Data.Starters[-1] and check_if_found_valid == False:
-                print("Not a proper 'gens'. Please choose valid 'gens'")
+    elif Awnser == "types":
+        while True:
+            check_if_found_valid = False
+            Type_categories = input("Use a valid type to get a list of pokemon in requested type. (gens 1-3) (press 'q' to quit): ").lower
+            if Type_categories == "q":
+                    break
+            for i in Data.Starters:
+                if i[2] == Type_categories:
+                    print(i[0])
+                    check_if_found_valid = True
+                elif i[2] != Type_categories and i == Data.Starters[-1] and check_if_found_valid == False:
+                    print("Not a valid type. Please choose valid types")
+    elif Awnser == "weaknesses":
+        Weaknesses_types=['weak to water, weak to grass, weak to fire']
+        while True:
+            Weaknesses = input(f"What type do you want to see the advantage of? {Weaknesses_types} (q to quit): \n").lower()
+            if Weaknesses == "q":
+                    break
+            for i in Data.Starters:
+                if i[4] == Weaknesses:
+                    print(i[0] + " is " + Weaknesses)
+                elif i[4] != Weaknesses and i == Data.Starters[-1]:
+                    print ("Not a valid weakness. Please choose a valid weakness")
+    elif Awnser == "generations":
+        while True:
+            check_if_found_valid = False
+            generations = input("Pick a generation for a list of pokemon. (gens 1-3) (press 'q' to quit): ").lower()
+            if generations == "q":
+                    break
+            for i in Data.Starters:
+                if i[3] == generations:
+                    print(i[0])
+                    check_if_found_valid = True
+                elif i[2] != generations and i == Data.Starters[-1] and check_if_found_valid == False:
+                    print("Not a proper 'gens'. Please choose valid 'gens'")
+    elif Awnser == "Exit":
+        break
