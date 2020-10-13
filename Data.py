b7e0af00b1dbda49210a1d1c9a671bd147136684
Gregory Bowles <Sbowles21@users.noreply.github.com>
2020-10-13 16:20:55 -0500

    Cleaning up

--- Data.py
+++ Data.py
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 Starters_string = [" bulbasaur,charmander,squirtle,chikorita,cyndaquil,totodile,treecko,torchic,mudkip"]
 bulbasaur=["bulbasaur",1,"grass","gen 1","weak to fire","evolves into Ivysaur", "While it is young, it uses the nutrients that are stored in the seed on its back in order to grow."]
 charmander=["charmander",4,"fire","gen 1","weak to water","evolves into Charmeleon","From the time it is born, a flame burns at the tip of its tail. Its life would end if the flame were to go out."]
@@ -9,3 +10,17 @@ treecko=["treecko",252,"grass", "gen 3","weak to fire","evolves into Grovyle","T
 torchic=["torchic",255,"fire", "gen 3" ,"weak to water","evolves into Combusken","torchic sticks with its trainer, following behind with unsteady steps.This pokémon breathes fire of over 1,800 degrees F, including fireballs that leave the foe scorched black."]
 mudkip=["mudkip",258,"water","gen 3","weak to grass","evolves into marshtomp","The fin on Mudkip’s head acts as highly sensitive radar. Using this fin to sense movements of water and air, this Pokémon can determine what is taking place around it without using its eyes."]
 Starters = [bulbasaur,charmander,squirtle,chikorita,cyndaquil,totodile,treecko,torchic,mudkip]
+=======
+Starters_string = ["bulbasaur","charmander","squirtle","chikorita","cyndaquil","totodile","treecko","torchic","mudkip"]
+bulbasaur=["bulbasaur", "1" ,"grass","1","fire", "While it is young, it uses the nutrients that are stored in the seed on its back in order to grow."]
+charmander=["charmander","4","fire","1","water","From the time it is born, a flame burns at the tip of its tail. Its life would end if the flame were to go out."]
+squirtle=["squirtle","7","water","1","grass","When it feels threatened, it draws its limbs inside its shell and sprays water from its mouth."]
+chikorita=["chikorita","152","grass","2","fire","In battle, Chikorita waves its leaf around to keep the foe at bay. However, a sweet fragrance also wafts from the leaf, becalming the battling Pokémon and creating a cozy, friendly atmosphere all around."]
+cyndaquil=["cyndaquil","155","fire", "2","water", "It is timid, and always curls itself up in a ball.If attacked, it flares up its back for protection"]
+totodile=["totodile","158","water", "2","grass", "Its well-developed jaws are powerful and capable of crushing anything.Even its trainer must be careful."]
+treecko=["treecko","252","grass", "3","fire", "Treeko has small hooks on the bottom of its feet that enable it to scale vertical walls.This pokémon attacks by slamming foes with its thick tail."]
+torchic=["torchic","255","fire", "3" ,"water","torchic sticks with its trainer, following behind with unsteady steps.This pokémon breathes fire of over 1,800 degrees F, including fireballs that leave the foe scorched black."]
+mudkip=["mudkip","258","water","3","grass", "The fin on Mudkip’s head acts as highly sensitive radar. Using this fin to sense movements of water and air, this Pokémon can determine what is taking place around it without using its eyes."]
+Starters = [bulbasaur,charmander,squirtle,chikorita,cyndaquil,totodile,treecko,torchic,mudkip]
+information = ["Name:", "Pokedex Number:", "Type:", "Generation:", "Weak to", "Pokedex Entry:" ]
+>>>>>>> Stashed changes
